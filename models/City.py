a72cee40c33fc10d43e10e4902e003baa908349c
vijay raagavan <raagavan.vijay@gmail.com>
2021-04-24 19:05:54 +0530

    [longitude,latitude]

--- models/City.py
+++ models/City.py
@@ -34,7 +34,7 @@ class City(object):
         self.name = data[CITY_FIELDS['name']]
         self.ascii_name = data[CITY_FIELDS['ascii_name']]
         self.alternate_names = data[CITY_FIELDS['alternate_names']].split(',')
-        self.location = { 'type':"Point", 'coordinates': [float(data[CITY_FIELDS['latitude']]), float(data[CITY_FIELDS['longitude']])] }
+        self.location = { 'type':"Point", 'coordinates': [float(data[CITY_FIELDS['longitude']]), float(data[CITY_FIELDS['latitude']])] }
         self.latitude = data[CITY_FIELDS['latitude']]
         self.longitude = data[CITY_FIELDS['longitude']]
         self.feature_class = data[CITY_FIELDS['feature_class']]
