90236b7fa879d0c8ff90b09ce93cc1190a6fea01
vijay raagavan <raagavan.vijay@gmail.com>
2021-04-24 19:04:15 +0530

    Fix formating

--- models/City.py
+++ models/City.py
@@ -34,7 +34,7 @@ class City(object):
         self.name = data[CITY_FIELDS['name']]
         self.ascii_name = data[CITY_FIELDS['ascii_name']]
         self.alternate_names = data[CITY_FIELDS['alternate_names']].split(',')
-        self.location = { 'type':"Point", 'coordinates': ["{0:.4f}".format(float(data[CITY_FIELDS['latitude']])), "{0:.4f}".format(float(data[CITY_FIELDS['longitude']]))] }
+        self.location = { 'type':"Point", 'coordinates': [float(data[CITY_FIELDS['latitude']]), float(data[CITY_FIELDS['longitude']])] }
         self.latitude = data[CITY_FIELDS['latitude']]
         self.longitude = data[CITY_FIELDS['longitude']]
         self.feature_class = data[CITY_FIELDS['feature_class']]
